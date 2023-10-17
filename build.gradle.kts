0ea6a0178a9dd662a7769cb8f31057941f3067a8
dont-doubt <74961444+dont-doubt@users.noreply.github.com>
2023-10-17 23:31:01 +0800

    Configured Gradle, Written README

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,17 @@
+/*
+ * ================================
+ *  Main Configuration
+ * ================================
+ */
+
 plugins {
-    id("java")
+    java
 }
 
 group = "neodym"
-version = "1.0-SNAPSHOT"
+version = "1.0.0"
+
+layout.buildDirectory = file("build")
 
 repositories {
     mavenCentral()
@@ -14,6 +22,48 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(8))
+        vendor.set(JvmVendorSpec.ORACLE)
+    }
+}
+
+tasks.processResources {
+    includeEmptyDirs = false
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+}
+
+
+/*
+ * ================================
+ *  Tasks
+ * ================================
+ */
+
 tasks.test {
     useJUnitPlatform()
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+}
+
+
+/*
+ * ================================
+ *  Extension methods
+ * ================================
+ */
+
+fun DependencyHandlerScope.preprocessor(dependency: String) {
+    annotationProcessor(dependency)
+    testAnnotationProcessor(dependency)
+}
+fun DependencyHandlerScope.compileTime(dependency: String) {
+    compileOnly(dependency)
+    testImplementation(dependency)
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,22 @@
+/*
+ * ================================
+ *  Out-of-subprojects Settings
+ * ================================
+ *
+ * FooJay repository allows you to automatically download targetting JDK if it absents
+ */
+
+@file:Suppress("UnstableApiUsage")
+
+import org.gradle.toolchains.foojay.FoojayToolchainResolver
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver") version "0.5.0"
+}
+
 rootProject.name = "Neodym"
+
+toolchainManagement.jvm.javaRepositories.repository("foojay") {
+    resolverClass.set(FoojayToolchainResolver::class.java)
+}
 
